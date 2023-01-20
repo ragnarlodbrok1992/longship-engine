45a44664fb8460af2c3169b32df3914edfdcd834
ragnarlodbrok1992 <maraus92@gmail.com>
2023-01-20 18:31:51 +0100

    Some changes to work around CL warning system

--- longship-engine.cpp
+++ longship-engine.cpp
@@ -1,9 +1,18 @@
 #define SDL_MAIN_HANDLED
 
+// Pragma workaround so we don't get errors from SDL headers
+#pragma warning(push, 0)
 #include <SDL2/SDL.h>
+#pragma warning(pop)
+
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
+  // Suppres warning for now - we are not going to use argc/argv for now.
+  (void) argc;
+  (void) argv;
+
   printf("Hello, Longship Engine!\n");
   return 0;
 }
