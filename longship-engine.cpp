cef9cd2a48c7afe2e8293738b149dd8cf36c5cbd
ragnarlodbrok1992 <maraus92@gmail.com>
2023-01-21 02:02:11 +0100

    Code to render line added

--- longship-engine.cpp
+++ longship-engine.cpp
@@ -6,6 +6,27 @@
 #pragma warning(pop)
 
 #include <stdio.h>
+#include <cstdint>
+
+typedef struct
+{
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
+  uint8_t A;
+} Color;
+
+typedef struct
+{
+  int x;
+  int y;
+} Point;
+
+void RenderLine(SDL_Renderer* renderer, Color color, Point start, Point end)
+{
+  SDL_SetRenderDrawColor(renderer, color.R, color.G, color.B, color.A);
+  SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+}
 
 int main(int argc, char* argv[])
 {
@@ -17,8 +38,9 @@ int main(int argc, char* argv[])
   printf("Version: pre-alpha\n");
 
   // Create objects for SDL
-  SDL_Window*  main_window  = NULL;
-  SDL_Surface* main_surface = NULL;
+  SDL_Window*   main_window   = NULL;
+  SDL_Surface*  main_surface  = NULL;
+  SDL_Renderer* main_renderer = NULL;
 
   // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -38,15 +60,27 @@ int main(int argc, char* argv[])
   // Initialize surface
   main_surface = SDL_GetWindowSurface(main_window);
 
-  // Proto main loop :3
-  SDL_FillRect(main_surface, NULL, SDL_MapRGB(main_surface->format, 0xFF, 0xFF, 0xFF));
-  SDL_UpdateWindowSurface(main_window);
+  // Initialize renderer
+  main_renderer = SDL_CreateRenderer(main_window, -1, SDL_RENDERER_ACCELERATED);
+
+  // Some objects to check rendering
+  static Point start_point = {100, 100};
+  static Point end_point = {200, 200};
+  static Color green = {0x00, 0xFF, 0x00, 0xFF};
 
   // Events
   SDL_Event main_event;
   bool quit_engine = false;
+
+  // Engine variables
+  const uint32_t frame_rate = 60;
+  const uint32_t frame_time = 1000 / frame_rate;
+
   while (!quit_engine)
   {
+    // Measure time at beginning of the frame
+    uint32_t start_time = SDL_GetTicks();
+
     while (SDL_PollEvent(&main_event))
     {
       switch (main_event.type)
@@ -69,6 +103,32 @@ int main(int argc, char* argv[])
             break;
           }
       }
+    }
+    // Event loop ends
+
+    // Clear the screen
+    SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, 255);
+    SDL_RenderClear(main_renderer);
+    
+    // Render something
+    RenderLine(main_renderer, green, start_point, end_point);
+
+    // Update screen
+    SDL_RenderPresent(main_renderer);
+
+    // Delay to control the frame rate
+    // TODO ragnar @improvement: is this a good idea to do just that?
+    // check how SDL_Delay works.
+    // Better to use SDL_AddTimer @maybe
+    // Measure time at the end of frame
+    uint32_t end_time = SDL_GetTicks();
+    uint32_t remaining_time = frame_time - (end_time - start_time);
+
+    // If there is time - delay
+    // TODO ragnar @improvement
+    if (remaining_time > 0)
+    {
+      SDL_Delay(remaining_time);
     }
   }
 
